2d3efa6f85d63479700449c377c20320712b7bc3
EC2 Default User <ec2-user@ip-172-31-6-62.ap-northeast-1.compute.internal>
2021-06-11 05:25:46 +0000

    add data

--- db/migrate/20210611050941_create_posts.rb
+++ db/migrate/20210611050941_create_posts.rb
@@ -1,7 +1,12 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :name
+      t.string :camera
+      t.string :lens
+      t.string :place
+      t.text :text
+      t.text :image
       t.timestamps
     end
   end
