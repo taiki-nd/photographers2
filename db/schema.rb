2bae751fc51d33d4678f3f3c5789957f38b0e512
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-13 13:06:29 +0000

    users show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_065816) do
+ActiveRecord::Schema.define(version: 2021_06_13_125504) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_065816) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "camera_maker"
     t.string "lens_maker"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
