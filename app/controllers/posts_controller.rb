5fbafbb759e99e28399f78062e4d22521590c6a2
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-15 14:51:45 +0000

    pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     
     def index
         #@posts = Post.all
-        @posts = Post.includes(:user)
+        @posts = Post.includes(:user).page(params[:page]).per(8).order('created_at DESC')
     end
     
     def new
