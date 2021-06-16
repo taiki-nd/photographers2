11acbb89be1ab10c02ac407ebdbb3d9033ab35e9
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-16 14:03:24 +0000

    comment destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,9 @@ class CommentsController < ApplicationController
     def destroy
         
     end
-    
+        @post = Post.find(params[:id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy 
     private
     def comment_params
         params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
