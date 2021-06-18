e67c9f34ec614460a720617df5253fa33200291b
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-18 13:12:02 +0000

    search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
     
     before_action :set_post, only: [:edit, :show]
-    before_action :move_to_index, except: [:index, :show]
+    before_action :move_to_index, except: [:index, :show, :search]
     
     
     def index
