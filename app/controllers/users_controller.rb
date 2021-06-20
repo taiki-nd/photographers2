00e162f26c3204b398102e91bd205c854ecfd1e8
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-20 11:34:37 +0000

    user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    
+    before_action :set_user, only: [:edit, :update]
+    
     def show
         #@posts = current_user.posts
         #@nickname = current_user.nickname
@@ -8,4 +11,24 @@ class UsersController < ApplicationController
         @posts = user.posts.page(params[:page]).per(8).order('created_at DESC')
         @nickname = user.nickname
     end
+    
+    def edit
+        unless @user == current_user
+            redirect_to user_path(@user)
+        end
+    end
+    
+    def update
+        if current_user.update(user_params)
+            redirect_to user_path(current_user)
+        else
+            redirect_to edit_user_path(current_user)
+        end
+    end
+    
+    private
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
 end
