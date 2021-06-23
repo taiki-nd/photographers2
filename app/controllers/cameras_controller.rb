2846e85326881bcb205cb0d0e778e1d0c80ea330
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-23 08:40:09 +0000

    create camera

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -5,7 +5,16 @@ class CamerasController < ApplicationController
     end
     
     def new
-        @post = Camera.new
+        @camera = Camera.new
+    end
+    
+    def create
+        Camera.create(camera_paramas)
+    end
+    
+    private
+    def camera_paramas
+        params.require(:camera).permit(:image, :camera_maker, :camera_name, :sensor_size, :pixels, :iso, :weight, :shooting_speed)
     end
     
 end
