eefd93fc31bef26767a3bcfd94b82e8f7090a5e3
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-25 08:45:12 +0000

    lenz create controller

--- app/controllers/lenzs_controller.rb
+++ app/controllers/lenzs_controller.rb
@@ -7,4 +7,14 @@ class LenzsController < ApplicationController
     def new
         @lenz = Lenz.new
     end
+    
+    def create
+        Lenz.create(lenz_params)
+    end
+    
+    private
+    def camera_paramas
+        params.require(:lenz).permit(:image, :lens_maker, :lens_name, :sensor_size, :f_number, :focal_length, :weight)
+    end
+    
 end
