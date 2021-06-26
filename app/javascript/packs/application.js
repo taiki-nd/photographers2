8c7d496a6b33c5af1a6b1778e706115e80a3cb52
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-26 05:54:03 +0000

    js star

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+window.$ = window.jQuery = require('jquery');
 require('jquery')
 require('index.js')
 
