b2d4d27dc3694c093921017873de5a7a6fc06d64
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-26 13:04:56 +0000

    revieww controller

--- app/controllers/reviewws_controller.rb
+++ app/controllers/reviewws_controller.rb
@@ -1,2 +1,20 @@
 class ReviewwsController < ApplicationController
+    
+    def create
+        review = Review.create(review_params)
+        redirect_to "/lenzs/#{review.lenz.id}"
+    end
+    
+    def destroy
+        @lenz = Lenz.find(params[:lenz_id])
+        review = @lenz.reviews.find(params[:id])
+        review.destroy 
+        redirect_to "/lenzs/#{review.lenz.id}"
+    end
+    
+    private
+    def review_params
+        params.require(:review).permit(:total_rate, :comfort_rate, :imgquality_rate, :cost_rate, :content).merge(user_id: current_user.id, lenz_id: params[:lenz_id])
+    end
+    
 end
