5250292a558d2824bb6133b6812a77b21a2cf1bb
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-26 06:00:13 +0000

    js star template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 window.$ = window.jQuery = require('jquery');
 require('jquery')
 require('index.js')
+require('packs/raty')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
