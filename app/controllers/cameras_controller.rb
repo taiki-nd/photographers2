e2110217a3719862827456de4afd130aa07a7c16
EC2 Default User <taiki.nd.real@gmail.com>
2021-06-28 13:26:16 +0000

    camera redirection

--- app/controllers/cameras_controller.rb
+++ app/controllers/cameras_controller.rb
@@ -1,5 +1,7 @@
 class CamerasController < ApplicationController
     
+    before_action :move_to_index, except: [:index, :show, :search]
+    
     def index
         @cameras = Camera.all.page(params[:page]).per(40).order('created_at DESC')
     end
@@ -36,6 +38,12 @@ class CamerasController < ApplicationController
     private
     def camera_paramas
         params.require(:camera).permit(:image, :camera_maker, :camera_name, :sensor_size, :pixels, :iso, :weight, :shooting_speed).merge(user_id: current_user.id)
+    end
+    
+    def move_to_index
+        unless current_user&.admin
+            redirect_to action: :index
+        end
     end
     
 end
