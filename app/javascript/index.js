a950bd6baa2d6695713d76f6433928422523f27b
EC2 Default User <taiki.nd.real@gmail.com>
2021-07-03 07:16:54 +0000

    js reload

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,17 +1,17 @@
-
-$(function(){
-
-  $('.toggle_btn').on('click', function(){
-    if (!$('#header').hasClass('open')){
-      $('#header').addClass('open');
-    }else{
+document.addEventListener("turbolinks:load", function () {
+  $(function(){
+  
+    $('.toggle_btn').on('click', function(){
+      if (!$('#header').hasClass('open')){
+        $('#header').addClass('open');
+      }else{
+        $('#header').removeClass('open');
+      }
+    });
+  
+    $('#mask').on('click', function(){ 
       $('#header').removeClass('open');
-    }
+    });
+  
   });
-
-  $('#mask').on('click', function(){ 
-    $('#header').removeClass('open');
-  });
-
 });
-
