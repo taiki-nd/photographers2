fa55bd291472220201977b0b0f668bcfc37ad58c
EC2 Default User <taiki.nd.real@gmail.com>
2021-07-05 06:53:30 +0000

    notice UI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   
+  before_action :move_to_notice, except: :notice
+  
   def company
   end
 
@@ -19,9 +21,21 @@ class PagesController < ApplicationController
     redirect_to "/pages/notice"
   end
   
+  def destroy
+    page = Page.find(params[:id])
+    page.destroy
+    redirect_to "/pages/notice"
+  end
+  
   private
     def page_params
         params.require(:page).permit(:twitter, :Date).merge(user_id: current_user.id)
+    end
+    
+    def move_to_notice
+      unless current_user&admin?
+        redirect_to "pages/notice"
+      end
     end
     
 end
