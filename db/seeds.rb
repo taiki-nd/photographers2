8a3a1d22e70322fadfe0f4ffd061605b93a28164
EC2 Default User <taiki.nd.real@gmail.com>
2021-07-06 05:42:15 +0000

    canon camera

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,12 @@ canon, nikon, sony, panasonic, olympus, 富士フイルム, pentax, sigma, hasse
         {name: 'その他'},
     ]    
 )
+
+['EOS-1D X Mark III', 'EOS-1D X Mark II', 'EOS-1D X', 
+ 'EOS R3', 'EOS R5', 'EOS R6', 'EOS R', 'EOS Ra', 
+ 'EOS 5D Mark IV', 'EOS 5D Mark III', 'EOS 5D Mark II', 'EOS 5Ds', 'EOS 5Ds R', 
+ 'EOS RP', 'EOS 6D mark II', 'EOS 90D', 
+ 'EOS M6 Mark II', 'EOS Kiss M2', 'EOS Kiss M', 'EOS M200', 'EOS M100', 'EOS Kiss X10i', 'EOS Kiss X10', 'EOS Kiss X9i', 
+ 'EOS 9000D', 'EOS Kiss X90'].each do |name|
+    canon.children.create(name: name)
+end
