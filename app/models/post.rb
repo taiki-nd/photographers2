ed3a2a400db1cc386b0c25a0b923332cb5d51038
EC2 Default User <taiki.nd.real@gmail.com>
2021-07-06 05:03:14 +0000

    add ancetry to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     has_many :comments
+    has_ancestry
     
     def self.search(search)
         if search != ""
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_083645) do
+ActiveRecord::Schema.define(version: 2021_07_06_050214) do
 
   create_table "cameras", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "camera_maker"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 2021_07_05_083645) do
     t.string "camera_maker"
     t.string "lens_maker"
     t.integer "user_id"
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_posts_on_ancestry"
   end
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
