d8a66a2c4d6002b4f63da565c348388f29f85cec
EC2 Default User <taiki.nd.real@gmail.com>
2021-07-20 12:48:50 +0000

    adjustment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     end
     
     def ajax
-        @camera_maker = Slectbox.find_by(name_cam: 'camera_maker')
+        @camera_maker = Slectbox.find_by(name_cam: 'camera_name')
     end
     
     private
